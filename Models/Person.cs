3603dc9d5e9ed23d9397a5e089ee544ffcd64bab
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-07-31 16:38:51 -0500

    Implemented PUT and DELETE into code

---
+++ Models/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace CoffeeShop.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public Person Parent { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# C-SQL_CoffeeShop
